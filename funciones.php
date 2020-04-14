a39836944f1be4aeb5d44d268cd17dc86c339cc1
christian-cell <61358110+christian-cell@users.noreply.github.com>
2020-04-14 17:28:56 +0200

    snap de la practica de php

---
+++ funciones.php
@@ -0,0 +1,5 @@
+<?php
+function saludar($persona, $mensaje = 'hola')
+{
+   echo'hola amiga';
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+function cuentaLetra($upgradeHub){
+    $contador = ['u','p','g','r','a','d','e','h','u','b'];
+    print_r(array_count_values($contador));
+}
+
+cuentaLetra('upgradeHub');
+
+
+
